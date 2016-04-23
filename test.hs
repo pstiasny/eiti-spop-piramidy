0523d2eb3cf6d297a1f1b4f25efb46f8a0eeb951
Paweł Stiasny <pawelstiasny@gmail.com>
2016-04-23 20:07:03 +0200

    add a QuickCheck generator for solvable puzzles

--- test.hs
+++ test.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 import Data.Maybe
 import Data.List
 import Control.Monad
@@ -12,20 +13,55 @@ arbPuzzle :: Int -> Gen Piramidy
 arbPuzzle size = do
   let makeContraints = do
         v <- vectorOf size (choose (1, size))
-        constrs <- mapM (\i -> elements [Just i, Nothing]) v
-        return constrs
+        mapM (\i -> elements [Just i, Nothing]) v
   [above, below, left, right] <- replicateM 4 makeContraints
   return $ Piramidy above below left right
-  
+
+-- Generator of solvable puzzles of given size.
+solvablePuzzle :: Int -> Gen Piramidy
+solvablePuzzle size = do
+  -- Generator of arbitrary legal boards
+  let anyBoard = do
+        let makeRow = shuffle [1..size]
+            rows = vectorOf size makeRow
+        suchThat rows $ (all isNonrepeatingLine . transpose)
+
+  -- Find number of pyramids seen anywhere on board's edges
+  let contraintsFor rows =
+        Piramidy
+          (map (Just . lineConstraintFor) columns)
+          (map (Just . lineConstraintFor) $ map reverse columns)
+          (map (Just . lineConstraintFor) rows)
+          (map (Just . lineConstraintFor) $ map reverse rows)
+        where columns = transpose rows
+
+  -- Remove constraints randomly
+  let reduceConstraints (Piramidy a b l r) = do
+        a' <- mapM re a
+        b' <- mapM re b
+        l' <- mapM re l
+        r' <- mapM re r
+        return $ Piramidy a' b' l' r'
+        where re (Just x) = do
+                elements [Just x, Nothing]
+              re Nothing = return Nothing
+
+  board <- anyBoard
+  let fullConstraints = contraintsFor board
+  reduceConstraints fullConstraints
+
 -- Check if a row or column adheres to a constraint on its
--- beginning, i.e. do I see i piramids from the point of list's
+-- beginning, i.e. do I see i pyramids from the point of list's
 -- beginning.  Always True if there is no constraint.
 lineConstraintHolds :: Maybe Int -> [Int] -> Bool
 lineConstraintHolds Nothing _ = True
-lineConstraintHolds (Just i) xs = inner 0 i xs
-  where inner _ 0 [] = True
-        inner _ _ [] = False
-        inner max i (h:t) = if h > max then inner h (i-1) t
+lineConstraintHolds (Just i) xs = lineConstraintFor xs == i
+
+-- How many pyrmids can be seen from the point of list's beginning.
+lineConstraintFor :: [Int] -> Int
+lineConstraintFor line = inner 0 0 line
+  where inner _ i [] = i
+        inner max i (h:t) = if h > max then inner h (i+1) t
                                        else inner max i t
 
 -- Check if every row and column adheres to the constraints
@@ -50,11 +86,27 @@ bruteforce p@(Piramidy a b l r) =
         allRows = permutations [1..size]
         size = length a
 
-main = do
-  verboseCheckWith
-    (stdArgs {maxSize=5, maxSuccess=200})
-    (\p ->
-      let bruteSolutions = bruteforce p
-      in  (length bruteSolutions <= 1) ==>
-            classify (null bruteSolutions) "no solution" $
-              solve p == listToMaybe bruteSolutions)
+prop_solveMatchesBruteForcedSolution = mapSize (min 4) $ \p ->
+  let bruteSolutions = bruteforce p
+  in  (length bruteSolutions <= 1) ==>
+        classify (null bruteSolutions) "no solution" $
+          solve p == listToMaybe bruteSolutions
+
+prop_solveReturnsSolutionWithinConstraints =
+  forAll (sized solvablePuzzle) $ \p ->
+    case solve p of
+      Just solution -> constraintsHold p solution
+      Nothing -> False
+
+prop_solveReturnsLegalBoard =
+  forAll (sized solvablePuzzle) $ \p ->
+    case solve p of
+      Just rows -> all isNonrepeatingLine rows &&
+                   all isNonrepeatingLine (transpose rows)
+      Nothing -> False
+
+
+return []  -- template haskell magic for quickcheck
+main =
+  -- we will make maxSize larger once an efficient algorithm is implemented
+  $forAllProperties (quickCheckWithResult stdArgs {maxSize=5, maxSuccess=200})
