c3627e373828bb44a2395f014edee345b27bca25
Paweł Stiasny <pawelstiasny@gmail.com>
2016-05-25 18:01:26 +0200

    add a pretty printer for solutions

--- piramidy.hs
+++ piramidy.hs
@@ -1,12 +1,40 @@
+import Control.Monad
+import Data.List (last)
 import System.Environment (getArgs)
+
 import Solver
 
 main = do
   args <- getArgs
-  if length args /= 1
+  if length args < 1
     then error "usage: piramidy <input file>"
     else do
-      let [fileName] = args
+      let fileName = last args
+          prettyPrinting = "--pretty" `elem` args
+
       description <- readFile fileName
-      let problem = read description :: Piramidy
-      print $ solve problem
+      let problem@(Piramidy above below left right) = read description :: Piramidy
+          solution = solve problem
+
+          prettyConstr (Nothing) = " "
+          prettyConstr (Just i) = show i
+
+      if prettyPrinting
+      then do
+        case solution of
+          Nothing -> putStrLn "no solution"
+          Just rows -> do
+            putStr " "
+            forM_ above (putStr . prettyConstr)
+            putStrLn ""
+
+            forM_ (zip3 left rows right) $ \(l, row, r) -> do
+                putStr $ prettyConstr l
+                forM_ row (putStr . show)
+                putStrLn $ prettyConstr r
+
+            putStr " "
+            forM_ below (putStr . prettyConstr)
+            putStrLn ""
+      else
+        print solution
