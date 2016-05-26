f5ccea93f0b82c3b13a385760055c96f0e21f90d
Paweł Stiasny <pawelstiasny@gmail.com>
2016-05-26 20:06:19 +0200

    implement basic backtracking

--- Solver.hs
+++ Solver.hs
@@ -1,39 +1,79 @@
-module Solver where
+module Solver (Piramidy (Piramidy), solve) where
+import Control.Monad
 import Data.List
 import Data.Maybe
+import Debug.Trace
 
 data Piramidy = Piramidy [Maybe Int] [Maybe Int] [Maybe Int] [Maybe Int]
   deriving (Show, Read)
 
+type PartialBoard = [(Int, Int, Int)]
+data Partial = Partial Int PartialBoard (Int, Int) deriving Show
+
 solve :: Piramidy -> Maybe [[Int]]
+solve (Piramidy [] [] [] []) = Just []
 solve (Piramidy above below left right) =
-  -- BEGIN STUB --
-  -- Zaślepkowe rozwiązanie.  Tu wstaw właściwy alogrytm.
-  listToMaybe $ bruteforce (Piramidy above below left right)
+  listToMaybe $ extractSolution <$> backtrack grow done reject apply empty
   where
-    lineConstraintHolds :: Maybe Int -> [Int] -> Bool
-    lineConstraintHolds Nothing _ = True
-    lineConstraintHolds (Just i) xs = inner 0 i xs
-      where inner _ 0 [] = True
-            inner _ _ [] = False
-            inner max i (h:t) = if h > max then inner h (i-1) t
-                                           else inner max i t
+    grow (Partial n board pos) = [1..n]
+    advance (x, y) = let (dx, x') = (x + 1) `quotRem` n in (x', y + dx)
+    done (Partial n _ (x, y)) = (x, y) == (0, n)
+    reject partial@(Partial n board (x,y)) value =
+      value `elem` row ||
+      value `elem` column ||
+      (if y == n-1
+        then rejectByEdgeConstraint (above !! x) (column ++ [value]) ||
+             rejectByEdgeConstraint (below !! x) (value:reverse (column))
+        else False) ||
+      (if x == n-1
+        then rejectByEdgeConstraint (left !! y) (row ++ [value]) ||
+             rejectByEdgeConstraint (right !! y) (value:reverse row)
+        else False)
 
-    constraintsHold :: Piramidy -> [[Int]] -> Bool
-    constraintsHold (Piramidy above below left right) rows =
-      all id $
-        zipWith lineConstraintHolds left rows ++
-        zipWith lineConstraintHolds right (map reverse rows) ++
-        zipWith lineConstraintHolds above columns ++
-        zipWith lineConstraintHolds below (map reverse columns)
-      where columns = transpose rows
+      where row = currentRow partial
+            column = currentColumn partial
+            rejectByEdgeConstraint e r = not $ lineConstraintHolds e r
+    apply p@(Partial n board pos) value =
+      Partial n (addToBoard p value) (advance pos)
+    empty = Partial n [] (0, 0)
+    n = length above
 
-    isNonrepeatingLine xs = [1..length xs] \\ xs == []
+backtrack :: (a -> [b]) -> (a -> Bool) -> (a -> b -> Bool) ->
+               (a -> b -> a) -> a -> [a]
+backtrack grow done reject apply partial = do
+  step <- grow partial
+  when (reject partial step) mzero
+  let partial' = apply partial step
+  if done partial'
+    then return partial'
+    else backtrack grow done reject apply partial'
 
-    bruteforce p@(Piramidy a b l r) =
-      filter (constraintsHold p) $ allMatrices
-      where allMatrices = filter nonrep $ sequence $ replicate size allRows
-            nonrep rows = all isNonrepeatingLine (transpose rows)
-            allRows = permutations [1..size]
-            size = length a
-  -- END STUB --
+extractRow board y =
+  map (\(_, _, v) -> v) $
+    sortBy (\(x, _, _) (x', _, _) -> compare x x') $ 
+    filter (\(_,y',_) -> y == y') board
+currentRow (Partial size board (x, y)) = extractRow board y
+currentColumn (Partial size board (x, y)) =
+  map (\(_, _, v) -> v) $ 
+    sortBy (\(_, y, _) (_, y', _) -> compare y y') $ 
+    filter (\(x',_,_) -> x == x') board
+addToBoard :: Partial -> Int -> PartialBoard
+addToBoard (Partial size board (x, y)) value = (x,y,value):board
+
+extractSolution :: Partial -> [[Int]]
+extractSolution (Partial size board _) = 
+  map (extractRow board) [0..size-1]
+
+-- Check if a row or column adheres to a constraint on its
+-- beginning, i.e. do I see i pyramids from the point of list's
+-- beginning.  Always True if there is no constraint.
+lineConstraintHolds :: Maybe Int -> [Int] -> Bool
+lineConstraintHolds Nothing _ = True
+lineConstraintHolds (Just i) xs = lineConstraintFor xs == i
+
+-- How many pyrmids can be seen from the point of list's beginning.
+lineConstraintFor :: [Int] -> Int
+lineConstraintFor line = inner 0 0 line
+  where inner _ i [] = i
+        inner max i (h:t) = if h > max then inner h (i+1) t
+                                       else inner max i t
