bbbacef09bff80c7f6fa363bbef54d1d3da3c9d7
Paweł Stiasny <pawelstiasny@gmail.com>
2016-05-28 18:23:29 +0200

    refactor tests

--- test.hs
+++ test.hs
@@ -25,7 +25,7 @@ solvablePuzzle size = do
   let anyBoard = do
         let makeRow = shuffle [1..size]
             rows = vectorOf size makeRow
-        suchThat rows $ (all isNonrepeatingLine . transpose)
+        suchThat rows (all isNonrepeatingLine . transpose)
 
   -- Find number of pyramids seen anywhere on board's edges
   let contraintsFor rows =
@@ -62,25 +62,65 @@ bruteforce p@(Piramidy a b l r) =
         allRows = permutations [1..size]
         size = length a
 
-prop_solveMatchesBruteForcedSolution = mapSize (min 4) $ \p ->
+noMoreThan = min
+
+-- Check if, for a random puzzle, solver's solutions matches that
+-- of an exhaustive search.  We limit puzzle size since it is
+-- restricted by exhauistive search speed.
+prop_solveMatchesBruteForcedSolution = mapSize (noMoreThan 4) $ \p ->
   let bruteSolutions = bruteforce p
+      solution = solve p
   in  (length bruteSolutions <= 1) ==>
         classify (null bruteSolutions) "no solution" $
-          solve p == listToMaybe bruteSolutions
+        counterexample ("incorrect solution: " ++ show solution) $
+          solution == listToMaybe bruteSolutions
 
-prop_solveReturnsSolutionWithinConstraints =
+-- Check if, for a solvable puzzle, solver returns a solution
+-- that matches constraints on puzzle's edges.  We limit maximum
+-- problem size as speed is restricted by the problem generator.
+prop_solveReturnsSolutionWithinConstraintsForSolvableBoards =
+  mapSize (noMoreThan 4) $
   forAll (sized solvablePuzzle) $ \p ->
-    case solve p of
-      Just solution -> constraintsHold p solution
-      Nothing -> False
+    trySolve p
+      (\board -> constraintsHold p board)
+      False
 
-prop_solveReturnsLegalBoard =
+-- Check if, for a any random puzzle, solver returns a solution
+-- that matches constraints on puzzle's edges if a solution is found.
+prop_solveReturnsSolutionWithinConstraints p =
+  trySolve p
+    (\board -> constraintsHold p board)
+    True
+
+-- Check if, for a solvable puzzle, solver returns a solution
+-- that conforms to the no repetetion of numbers per row/column
+-- rule.  We limit maximum problem size as speed is restricted by
+-- the problem generator.
+prop_solveReturnsLegalBoardForSolvableBoards =
+  mapSize (noMoreThan 4) $
   forAll (sized solvablePuzzle) $ \p ->
-    case solve p of
-      Just rows -> all isNonrepeatingLine rows &&
-                   all isNonrepeatingLine (transpose rows)
-      Nothing -> False
+    trySolve p
+      (\rows -> all isNonrepeatingLine rows &&
+                all isNonrepeatingLine (transpose rows))
+      False
 
+-- Check if, for any random puzzle, solver returns a solution
+-- that conforms to the no repetetion of numbers per row/column
+-- rule if a solution is found.
+prop_solveReturnsLegalBoard p =
+  trySolve p
+    (\rows -> all isNonrepeatingLine rows &&
+              all isNonrepeatingLine (transpose rows))
+    True
+
+trySolve p onJust onNothing =
+  let solution = solve p
+  in
+    classify (isNothing solution) "no solution" $
+    counterexample ("incorrect solution: " ++ show solution) $
+    case solution of
+      Just board -> onJust board
+      Nothing -> onNothing
 
 return []  -- template haskell magic for quickcheck
 main =
