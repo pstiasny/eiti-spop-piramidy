c5333de4d3270ac8ad1c1b79627075e1dbd4f767
Paweł Stiasny <pawelstiasny@gmail.com>
2016-05-29 02:10:05 +0200

    implement recursive elimination when a singular option is found

--- Elimination.hs
+++ Elimination.hs
@@ -17,27 +17,34 @@ eliminate (Piramidy above below left right) =
       board <- MV.replicate (size * size) [1..size]
 
       let
-        -- indexing within rows and columns
-        row board y = (board, \x -> index x y)
-        column board x = (board, index x)
-        flip (board, f) = (board, \x -> f (size - x - 1))
-
         -- row/column accessors and modifiers
-        modify (board, index) f x = MV.modify board f (index x)
-        {-read row x = MV.read board (row x)-}
+        read x y = MV.read board (index x y)
+        write x y v = MV.write board (index x y) v
 
-        eliminateRow Nothing _ = return ()
-        eliminateRow (Just edgeNumber) row = do
-          forM_ [1..edgeNumber - 1] $ \i -> do
-            modify row (\\ [i + size + 1 - edgeNumber..size]) (i - 1)
+        rid badOptions x y = do
+          options <- read x y
+          let options' = options \\ badOptions
+          write x y options'
+          when (options /= options' && length options' == 1) $ do
+            forM_ [0..x - 1] $ \x -> rid options' x y
+            forM_ [x + 1..size-1] $ \x -> rid options' x y
+            forM_ [0..y - 1] $ \y -> rid options' x y
+            forM_ [y + 1..size-1] $ \y -> rid options' x y
 
-      forM_ (zip left [0..size-1]) $ \(edge, y) ->
-        eliminateRow edge (row board y)
-      forM_ (zip right [0..size-1]) $ \(edge, y) ->
-        eliminateRow edge (flip $ row board y)
-      forM_ (zip above [0..size-1]) $ \(edge, x) ->
-        eliminateRow edge (column board x)
-      forM_ (zip below [0..size-1]) $ \(edge, x) ->
-        eliminateRow edge (flip $ column board x)
+        eliminateRow edgeNumber row = do
+          forM_ row $ \(i, x, y) -> do
+            rid [i + size + 1 - edgeNumber..size] x y
+
+      forM_ (zip left [0..size-1]) $ \(mk, y) -> maybeDo mk $ \k ->
+        eliminateRow k [(i, i - 1, y) | i <- [1..k - 1]]
+      forM_ (zip right [0..size-1]) $ \(mk, y) -> maybeDo mk $ \k ->
+        eliminateRow k [(i, size - i, y) | i <- [1..k - 1]]
+      forM_ (zip above [0..size-1]) $ \(mk, x) -> maybeDo mk $ \k ->
+        eliminateRow k [(i, x, i - 1) | i <- [1..k - 1]]
+      forM_ (zip below [0..size-1]) $ \(mk, x) -> maybeDo mk $ \k ->
+        eliminateRow k [(i, x, size - i) | i <- [1..k - 1]]
       return board
   in \(x, y) -> v V.! index x y
+
+maybeDo (Just x) f = f x
+maybeDo Nothing _ = return ()
