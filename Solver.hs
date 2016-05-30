613d432ff89bf621f38c6dca195a81ca0121a5fa
Paweł Stiasny <pawelstiasny@gmail.com>
2016-05-31 01:14:38 +0200

    add comments for the Solver module

--- Solver.hs
+++ Solver.hs
@@ -7,26 +7,43 @@ import Data.Maybe
 import Problem
 import Elimination
 
-type PartialBoard = ([[Int]], Map.Map Int [Int])
+-- Partial solution of the pyramid puzzle.
+-- The data structure is optimized for fast lookup when validating
+-- consecutive solutions.
 data Partial =
-  Partial Int PartialBoard (Int, Int) ([Maybe Int], [Maybe Int], [Maybe Int], [Maybe Int])
+  Partial
+    Int  -- puzzle size
+    PartialBoard -- board so far
+    (Int, Int) -- coordinates for insertion of the next pyramid
+    ([Maybe Int], [Maybe Int], [Maybe Int], [Maybe Int]) -- active edge constraints
   deriving Show
+-- The partial board is represented as a list of rows and a map of columns.
+-- When applying a step, we append a pyramid to a row and update a column.
+type PartialBoard = ([[Int]], Map.Map Int [Int])
 
 solve :: Piramidy -> Maybe [[Int]]
 solve (Piramidy [] [] [] []) = Just []
 solve problem@(Piramidy above below left right) =
   listToMaybe $ extractSolution <$> backtrack grow done reject apply empty
   where
+    -- generate possible steps
     grow (Partial _ _ pos _) = options pos
+    -- next coordinates
     advance (x, y) = let (dx, x') = (x + 1) `quotRem` n in (x', y + dx)
+    -- is the solution complete?
     done (Partial n _ (x, y) _) = (x, y) == (0, n)
+    -- should the step be rejected?
     reject partial@(Partial n board (x,y) (a:_, b:_, l:_, r:_)) value =
+      -- pyramid has not yet been used in current row
       value `elem` row ||
+      -- pyramid has not yet been used in current column
       value `elem` column ||
+      -- column has the correct number of pyramids seen from both edges
       (if y == n-1
         then rejectByEdgeConstraint b (value:column) ||
              rejectByEdgeConstraint a (reverse $ value:column)
         else False) ||
+      -- row has the correct number of pyramids seen from both edges
       (if x == n-1
         then rejectByEdgeConstraint r (value:row) ||
              rejectByEdgeConstraint l (reverse $ value:row)
@@ -35,15 +52,19 @@ solve problem@(Piramidy above below left right) =
       where row = currentRow partial :: [Int]
             column = currentColumn partial :: [Int]
             rejectByEdgeConstraint e r = not $ lineConstraintHolds e r
+    -- apply a step to a partial solution, creating a larger partial solution
     apply p@(Partial n board pos (_:as, _:bs, _:ls, _:rs)) value =
       Partial n (addToBoard p value) (advance pos) (as, bs, ls, rs)
+    -- empty partial solution
     empty = Partial n (emptyBoard n) (0, 0)
                       (cycle above, cycle below,
                        cycle (left >>= (replicate n)),
                        cycle (right >>= (replicate n)))
+
     options = eliminate problem :: (Int, Int) -> [Int]
     n = length above
 
+-- General scheme for backtracking.
 backtrack :: (a -> [b]) -> (a -> Bool) -> (a -> b -> Bool) ->
                (a -> b -> a) -> a -> [a]
 backtrack grow done reject apply partial = do
@@ -54,6 +75,7 @@ backtrack grow done reject apply partial = do
     then return partial'
     else backtrack grow done reject apply partial'
 
+-- accessors and modifiers for partial solutions
 currentRow :: Partial -> [Int]
 currentRow (Partial _ ([], _) _ _) = []
 currentRow (Partial _ _ (0, _) _) = []
